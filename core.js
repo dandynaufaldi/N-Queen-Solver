da56a40c186d20f7aab2ce9b95ec10fd72b8147c
Dandy Naufaldi <dandy.naufaldi@gmail.com>
2018-04-03 11:46:05 +0700

    Auto stash before merge of "master" and "origin/master"

---
+++ core.js
@@ -0,0 +1,36 @@
+/*Object mycanvas
+Attr:
+- canvas chess
+- ‎canvas line graph
+
+Method :
+- start
+- ‎reset
+
+Object : chessboard
+Attr :
+- 1D arr queen 
+- ‎Mode
+- ‎Algorithm option
+- ‎2D arr chess board (heuristic value in canvas)
+- ‎curr queen
+- ‎curr row
+- ‎step counter
+- ‎arr heuristic history
+
+Method
+- check col
+- ‎check row
+- ‎check diaPos
+- ‎check diaNeg
+- ‎getHeuristic
+- ‎makemove
+- ‎run (move count, makemove, getheur, show heur 1 queen, select n mark min, from all choose best, update queen pos) based on algo
+- ‎draw line graph*/
+
+$(document).ready(function(){
+	var chessCanvas = document.getElementById("chess-canvas");
+	var lineCanvas = document.getElementById("line-canvas");
+
+	var 
+});
